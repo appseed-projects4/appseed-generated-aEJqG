e7ef2b8ce1ed1135be79452d10e3dff929d4c673
AppSeed Generated Projects <133563814+appseed-projects4@users.noreply.github.com>
2024-06-16 16:07:14 +0300

    initial commit

---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# Install modules 
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+
+# Collect Static
+python manage.py collectstatic --no-input
+
+# Migrate DB 
+python manage.py makemigrations
+python manage.py migrate
+
+# Provisioning  
+python manage.py seeder_tags
+python manage.py seeder_skills
+
+# Generate DOCS (DEPRECATED)
+#python manage.py build_docs
+
+# Generate Sphinx DOCS
+cd docs && make html
